b96ae92cd21709146cce3186e5f4e0a62b45b5e8
Manas-Nanivadekar <manasnanivadekar@gmail.com>
2025-06-27 10:19:14 +0530

    updated dataloader for fineweb

--- main.py
+++ main.py
@@ -248,25 +248,39 @@ max_length = 30
 # prefix tokens
 import tiktoken
 import time
+import numpy as np
 
 device = "cuda"
 
 
+def load_tokens(filename):
+    npt = np.load(filename)
+    ptt = torch.tensor(npt, dtype=torch.long)
+    return ptt
+
+
 class DataLoaderLite:
-    def __init__(self, B, T, process_rank, num_proccesses):
+    def __init__(self, B, T, process_rank, num_proccesses, split):
         self.B = B
         self.T = T
         self.process_rank = process_rank
         self.num_proccesses = num_proccesses
+        assert split in {"train", "val"}
 
         # at init load tokens from disk and store them in memory
-        with open("input.txt", "r") as f:
-            text = f.read()
-        enc = tiktoken.get_encoding("gpt2")
-        tokens = enc.encode(text)
-        self.tokens = torch.tensor(tokens)
-        print(f"loaded {len(self.tokens)} tokens")
-        print(f"1 epoch = {len(self.tokens) // (B*T)} batches")
+        data_root = "edu_fineweb10B"
+        shards = os.listdir(data_root)
+        shards = [s for s in shards if split in s]
+        shards = sorted(shards)
+        shards = [os.path.join(data_root, s) for s in shards]
+        self.shards = shards
+        assert len(shards) > 0, f"no shards found for split {split}"
+        if master_process:
+            print(f"found {len(shards)} shards for split {split}")
+
+        # state, init at shard zero
+        self.current_shard = 0
+        self.tokens = load_tokens(self.shards[self.current_shard])
         self.current_position = self.B * self.T * self.process_rank
 
     def next_batch(self):
@@ -318,7 +332,7 @@ if master_process:
     print(f"=> calculated gradient accumulation steps: {grad_accum_steps}")
 
 train_loader = DataLoaderLite(
-    B=B, T=T, process_rank=ddp_rank, num_proccesses=ddp_world_size
+    B=B, T=T, process_rank=ddp_rank, num_proccesses=ddp_world_size, split="train"
 )
 torch.set_float32_matmul_precision("high")
 
@@ -333,8 +347,8 @@ raw_model = model.module if ddp else model
 
 max_lr = 6e-4
 min_lr = max_lr * 0.1
-warmup_steps = 10
-max_steps = 50
+warmup_steps = 715
+max_steps = 19073
 
 
 def get_lr(it):
